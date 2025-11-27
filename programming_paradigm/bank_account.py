b177f396fc180b70d74151a7c3622f7241718030
OojayFidel <odeyjonathan008@gmail.com>
2025-11-27 14:26:50 +0100

    Week 6 first task

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.balance = account_balance
+               
+    def deposit(self, amount):
+       if amount > 0:
+            self.balance += amount
+            return True
+       return False
+        
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
+        
+    def display_balance(self):
+        return self.balance
+        print(f"Current Balance: ${self.balance}")
+        
